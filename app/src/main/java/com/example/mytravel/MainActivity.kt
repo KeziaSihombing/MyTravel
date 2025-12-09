691ed83bfab3ec7fb8c4b7452632acbd1e5def81
Daniel <irfan2904.ia@gmail.com>
2025-12-09 13:20:16 +0700

    merge master into er

--- app/src/main/java/com/example/mytravel/MainActivity.kt
+++ app/src/main/java/com/example/mytravel/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.mytravel.ui.budget.BudgetScreen
+import com.example.mytravel.ui.pages.LoginScreen
+import com.example.mytravel.ui.pages.RegisterScreen
 import com.example.mytravel.ui.theme.MyTravelTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,5 +22,14 @@ class MainActivity : ComponentActivity() {
                 BudgetScreen()
             }
         }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DefaultPreview() {
+    MyTravelTheme {
+        // Langsung menampilkan BudgetScreen untuk pengujian
+        BudgetScreen()
     }
 }
