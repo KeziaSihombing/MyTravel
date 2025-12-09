a3571f9ce3736a299cb770239638d34fd760e7ad
Kezia Sihombing <keziasihombing184@gmail.com>
2025-12-09 09:45:23 +0700

    update

--- app/src/main/java/com/example/mytravel/ui/pages/ListCommentsScreen.kt
+++ app/src/main/java/com/example/mytravel/ui/pages/ListCommentsScreen.kt
@@ -1,16 +1,30 @@
 package com.example.mytravel.ui.pages
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Comment
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mytravel.domain.model.CommentWithUserName
 import com.example.mytravel.ui.common.UiResult
@@ -20,7 +34,8 @@ import com.example.mytravel.ui.viewmodel.ListCommentViewModel
 @Composable
 fun ListCommentsScreen(
     viewModel: ListCommentViewModel = viewModel(),
-    onNavigateBack: () -> Unit
+    onNavigateBack: () -> Unit,
+    onNavigateAddComment: () -> Unit
 ) {
     val comments by viewModel.comments.collectAsState()
     LaunchedEffect(Unit) {
@@ -33,7 +48,41 @@ fun ListCommentsScreen(
             CircularProgressIndicator()
         }
         is UiResult.Success -> {
-            CommentList((comments as UiResult.Success).data)
+            Row (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+            ){
+                Row(
+                    modifier = Modifier
+                        .clickable { onNavigateBack() },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.ArrowBackIosNew, contentDescription = null, modifier = Modifier.size(20.dp))
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "Kembali",
+                        fontSize = 20.sp,
+                        color = Color.Gray
+                    )
+                }
+                Spacer(modifier = Modifier.width(15.dp))
+
+                Icon(
+                    Icons.Default.Comment,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clickable { onNavigateAddComment() }
+                )
+                Text(
+                    text = "Komentar",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.width(15.dp))
+                CommentList((comments as UiResult.Success).data)
+            }
         }
         is UiResult.Error -> {
             Text("Error: ${(comments as UiResult.Error).message}")
